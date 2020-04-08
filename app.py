84689b778f17a0355a14800fec89625766ea82c3
ghanendrapiplani <ghanendrapiplani@gmail.com>
2020-04-07 21:28:36 -0400

    mds plot

--- app.py
+++ app.py
@@ -11,9 +11,32 @@ from sklearn import decomposition
 from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import random
+from sklearn import manifold
+from sklearn.metrics import pairwise_distances
 
 # First of all you have to import it from the flask module:
 app = Flask(__name__)
+
+base_cols = ['AUSTRALIA - AUSTRALIAN DOLLAR/US$', 'EURO AREA - EURO/US$',
+             'NEW ZEALAND - NEW ZELAND DOLLAR/US$',
+             'UNITED KINGDOM - UNITED KINGDOM POUND/US$', 'BRAZIL - REAL/US$',
+             'CANADA - CANADIAN DOLLAR/US$', 'CHINA - YUAN/US$',
+             'HONG KONG - HONG KONG DOLLAR/US$', 'INDIA - INDIAN RUPEE/US$',
+             'KOREA - WON/US$', 'MEXICO - MEXICAN PESO/US$',
+             'SOUTH AFRICA - RAND/US$', 'SINGAPORE - SINGAPORE DOLLAR/US$',
+             'DENMARK - DANISH KRONE/US$', 'JAPAN - YEN/US$',
+             'MALAYSIA - RINGGIT/US$', 'NORWAY - NORWEGIAN KRONE/US$',
+             'SWEDEN - KRONA/US$', 'SRI LANKA - SRI LANKAN RUPEE/US$',
+             'SWITZERLAND - FRANC/US$', 'TAIWAN - NEW TAIWAN DOLLAR/US$',
+             'THAILAND - BAHT/US$']
+
+renamed_cols = ['AUSTRALIA_AUSDOLLARS', 'EUROPE_EURO', 'NEWZEALAND_NZDOLLAR',
+                'UNITEDKINGDOM_POUND', 'BRAZIL_REAL', 'CANADA_CANADIANDOLLAR',
+                'CHINA_YUAN', 'HONGKONG_HKDOLLAR', 'INDIA_INDIANRUPEE', 'KOREA_WON',
+                'MEXICO_PESO', 'SOUTHAFRICA_RAND', 'SINGAPORE_SINGAPOREDOLLAR',
+                'DENMARK_DANISHKRONE', 'JAPAN_YEN', 'MALAYSIZ_RINGGIT', 'NORWAY_NORWAYKRONE',
+                'SWEDEN_SWEDISHKRONE', 'SRILANKA_SRILANKANRUPEE', 'SWITZERLAND_FRANC', 'TAIWAN_NEWTAIWANDOLLAR',
+                'THAILAND_BAHT']
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -57,7 +80,6 @@ def plot_k(k, x):
     print(df_kcluster1.shape)
     print(df_kcluster2.shape)
     df_ = pd.concat([df_kcluster0, df_kcluster1, df_kcluster2])
-    # print(df_)
     return df_
 
 
@@ -111,7 +133,7 @@ def generate_eigenValues(data):
 
 def plot_intrinsic_dimensionality_pca(data, k):
     [eigenValues, eigenVectors] = generate_eigenValues(data)
-    print ("eigenValues")
+    print("eigenValues")
     squaredLoadings = []
     ftrCount = len(eigenVectors)
     for ftrId in range(0, ftrCount):
@@ -141,11 +163,12 @@ def pca(df_=None):
 def process_val(val):
     if val == "ND":
         return 0.0
-    else: return float(val)
+    else:
+        return float(val)
 
 
 @app.route('/scatter_matrix_random')
-def scatter_plot():
+def scatter_matrix_plot():
     global df_main
     df_ = plot_k(3, df_main)
     squared_loadings = plot_intrinsic_dimensionality_pca(df_, 3)
@@ -160,15 +183,103 @@ def scatter_plot():
     print(data_columns)
     csv = data_columns.to_csv("csv")
     print(csv)
-    return render_template("scatter.html", data=csv)
+    return render_template("scatter_matrix.html", data=csv)
+
+
+@app.route('/scatter_2d_fulldata')
+def scatter_fulldata_plot():
+    global df_main
+    pca_data = PCA(n_components=2)
+    pca_data.fit(df_main)
+    df_ = pca_data.transform(df_main)
+    data_final = pd.DataFrame(df_)
+    print("shapes {} {}".format(df_main.shape, data_final.shape))
+    print(data_final.head)
+    print(df_main.head)
+    return render_template("scatter_2d.html", data=json.dumps(data_final.to_dict()))
+
+
+@app.route('/scatter_2d_random')
+def scatter_random_plot():
+    global df_main_random
+    pca_data = PCA(n_components=2)
+    pca_data.fit(df_main_random)
+    df_ = pca_data.transform(df_main_random)
+    data_final = pd.DataFrame(df_)
+    print("shapes {} {}".format(df_main_random.shape, data_final.shape))
+    print(data_final.head)
+    print(df_main_random.head)
+    return render_template("scatter_2d.html", data=json.dumps(data_final.to_dict()))
+
+
+@app.route('/scatter_2d_kmeans')
+def scatter_kmeans_plot():
+    global df_main_kmeans
+    pca_data = PCA(n_components=2)
+    pca_data.fit(df_main_kmeans)
+    df_ = pca_data.transform(df_main_kmeans)
+    data_final = pd.DataFrame(df_)
+    print("shapes {} {}".format(df_main_kmeans.shape, data_final.shape))
+    print(data_final.head)
+    print(df_main_kmeans.head)
+    data_final['clusterid'] = np.nan
+    x = 0
+    for index, row in df_main_kmeans.iterrows():
+        data_final['clusterid'][x] = row['cluster_num']
+        x = x + 1
+    return render_template("scatter_2d.html", data=json.dumps(data_final.to_dict()))
+
+
+@app.route('/mds_euclidean_full')
+def mds_euclidean_full():
+    global df_main
+    df_full_subset = df_main.sample(n=int(0.2 * len(df_main)))
+    print("dfdf {}".format(df_full_subset.shape))
+    mds_data = manifold.MDS(n_components=2, dissimilarity='precomputed')
+    similarity = pairwise_distances(df_full_subset, metric='euclidean')
+    X = mds_data.fit_transform(similarity)
+    data_final = pd.DataFrame(X)
+    return render_template("scatter_2d.html", data=json.dumps(data_final.to_dict()))
+
+
+@app.route('/mds_euclidean_random')
+def mds_euclidean_random():
+    global df_main_random
+    df_random_subset = df_main_random.sample(n=int(0.2 * len(df_main_random)))
+    print("dfdf {}".format(df_random_subset.shape))
+    mds_data = manifold.MDS(n_components=2, dissimilarity='precomputed')
+    similarity = pairwise_distances(df_random_subset, metric='euclidean')
+    X = mds_data.fit_transform(similarity)
+    data_final = pd.DataFrame(X)
+    return render_template("scatter_2d.html", data=json.dumps(data_final.to_dict()))
+
+
+@app.route('/mds_euclidean_kmeans')
+def mds_euclidean_kmeans():
+    global df_main_kmeans
+    df_kmeans_subset = df_main_kmeans.sample(n=int(0.2 * len(df_main_kmeans)))
+    print("dfdf {}".format(df_kmeans_subset.shape))
+    mds_data = manifold.MDS(n_components=2, dissimilarity='precomputed')
+    similarity = pairwise_distances(df_kmeans_subset, metric='euclidean')
+    X = mds_data.fit_transform(similarity)
+    data_final = pd.DataFrame(X)
+    data_final['clusterid'] = np.nan
+    x = 0
+    for index, row in df_kmeans_subset.iterrows():
+        data_final['clusterid'][x] = row['cluster_num']
+        x = x + 1
+    return render_template("scatter_2d.html", data=json.dumps(data_final.to_dict()))
 
 
 if __name__ == "__main__":
     df = pd.read_csv('forex_rates.csv')
     df_main = df.drop("Time Serie", axis=1)
+    df_main.columns = renamed_cols
     print(df_main.columns)
     for c in df_main.columns:
         df_main[c] = df_main[c].map(lambda val: process_val(val))
     scaler = StandardScaler()
     df_main[df_main.columns] = scaler.fit_transform(df_main[df_main.columns])
+    df_main_random = df_main.sample(n=int(0.25 * len(df_main)))
+    df_main_kmeans = plot_k(3, df_main)
     app.run(debug=True)
